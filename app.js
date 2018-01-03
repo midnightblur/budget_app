9075585f42d44e4025d15e52ef4fcbcab6d6d352
Quoc Minh VU <minhvq27@gmail.com>
2018-01-03 18:53:53 -0500

    MVC architecture init

---
+++ app.js
@@ -0,0 +1,24 @@
+/**
+ * The model
+ */
+var theModel = (function() {
+
+    // Expose public API
+    return {
+
+    };
+}());
+
+/**
+  * The view
+  */
+var theView = (function() {
+    // Code update or read the UI
+}());
+
+/**
+ * The controller
+ */
+var theController = (function(model, view) {
+    // Controller code
+}(theModel, theView));
