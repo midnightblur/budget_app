9a978f2095858214dbc43c4b3e17d31a59e3fb5f
Quoc Minh VU <minhvq27@gmail.com>
2018-01-04 17:50:59 -0500

    Get input data

--- app.js
+++ app.js
@@ -13,12 +13,61 @@ var theModel = (function() {
   * The view
   */
 var theView = (function() {
-    // Code update or read the UI
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDesc: '.add__description',
+        intputValue: '.add__value',
+        inputBtn: '.add__btn',
+    };
+
+    return {
+        getInput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // Value is either 'inc' or 'exp'
+                description: document.querySelector(DOMstrings.inputDesc).value,
+                amount: document.querySelector(DOMstrings.intputValue).value,
+            };
+        },
+
+        clearInput: function() {
+            document.querySelector(DOMstrings.inputDesc).value = '';
+            document.querySelector(DOMstrings.intputValue).value = '';
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+    };
 }());
 
 /**
  * The controller
  */
 var theController = (function(model, view) {
-    // Controller code
+    var DOMstrings = theView.getDOMstrings();
+
+    /**
+     * The function to read the input from UI and handle it properly
+     */
+    var ctrlAddItem = function() {
+        // Get the input data
+        var input = theView.getInput();
+        theView.clearInput();
+
+        // Add the item to the model
+
+        // Add a new item to the UI
+
+        // Calculate the budget
+
+        // Display the budget
+    };
+
+    document.querySelector(DOMstrings.inputBtn).addEventListener('click', ctrlAddItem);
+
+    document.addEventListener('keypress', function(event) {
+        if (event.keyCode === 13 || event.which === 13) { // If pressed key is the Return key
+            ctrlAddItem();
+        }
+    });
 }(theModel, theView));
